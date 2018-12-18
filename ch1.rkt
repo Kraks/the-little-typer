eff27f0e2f2db47763d901c590751580fdbd6db3
Guannan Wei <kiss.kraks@gmail.com>
2018-12-18 01:02:58 -0500

    misc

--- ch1.rkt
+++ ch1.rkt
@@ -1,5 +1,20 @@
 #lang pie
 
+'atom
+'ratatouille
+'---
+
+(the (Pair Atom Atom)
+  (cons 'ratatouille 'baguette))
+
+(Pair Atom Atom)
+
+(car (the (Pair Atom Atom)
+       (cons 'ratatouille 'baguette)))
+
+(cdr (the (Pair Atom Atom)
+       (cons 'ratatouille 'baguette)))
+
 (claim one Nat)
 (define one
   (add1 zero))
